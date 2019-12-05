17502514de6093a62aa903f7cfbeb9dc75bc7bcf
Thomas <53497553+thomasandrew@users.noreply.github.com>
2019-12-04 22:22:21 -0300

    Create array.php

---
+++ array/array.php
@@ -0,0 +1,20 @@
+<?php
+  
+  $names = [
+      
+    'Jeff',
+    'John',
+    'Mary'
+
+  ];
+
+  /*foreach ($names as $name) { // Vai para até o ultimo vetor com esse nome $name.
+      // echo $name; Vai printar todos os vetores.
+
+      echo $name . ','; // Vai adicionar uma virgula.
+
+  } */
+
+  require 'arraypt2.php';
+ 
+?>
