dc7d56688ce6979768ed5f422682de05c8d02129
Thomas <53497553+thomasandrew@users.noreply.github.com>
2019-12-09 15:48:44 -0300

    Add files via upload

---
+++ function/function_homework.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'funtion_homework2.php'; // Sempre coloca antes.
+
+$person = 21;
+
+club($person);
---
+++ function/funtion_homework2.php
@@ -0,0 +1,9 @@
+<?php
+
+function club($age) {
+    if ($age >= 21) {
+        echo true;
+    } else {
+        echo false;
+    }
+}
