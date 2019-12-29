83da3ae7ef1c22806fcfc180a0949e078ba13f72
Thomas <53497553+thomasandrew@users.noreply.github.com>
2019-12-29 13:26:50 -0300

    Create exercise3.php

---
+++ array/exercise/exercise3.php
@@ -0,0 +1,35 @@
+<?php
+
+$ceu = array(
+
+ "italy" => "rome",
+ "luxembourg" => "copenhagen",
+ "finland" => "helsinki",
+ "france" => "paris",
+ "sloviakia" => "vratislava",
+ "slovenia" => "ljublijana",
+ "germany" => "berkin",
+ "greece" => "athens",
+ "ireland" => "dublin",
+ "netherlands" => "amsterdam",
+ "portugual" => "lisbon",
+ "spain" => "madrid",
+ "sweden" => "stokholm",
+ "united kingdom" => "london",
+ "cyprus" => "nicosia",
+ "lithuania" => "vilnius",
+ "czech replubic" => "prague",
+ "estonia" => "tallin",
+ "hungary" => "budapest",
+ "latvia" => "riga",
+ "malta" => "valetta",
+ "austria" => "vienna",
+ "poland" => "warsaw"
+
+);
+
+foreach ($ceu as $ceus => $contry) {
+    echo "The capital of {$ceus} is {$contry} <br>";
+}
+
+
