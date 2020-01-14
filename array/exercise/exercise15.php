cddf49022473a87a407977903a01c2f416c89e4e
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-01-13 22:03:42 -0300

    Update exercise15.php

--- array/exercise/exercise15.php
+++ array/exercise/exercise15.php
@@ -1,8 +1,8 @@
 <?php
 
-$rand = range(11, 20);
+$rand = range(11, 20); // Eu acho que range deve ter alguma coisa a ver com array.
 
-shuffle($rand); // Para dar um aleatorio de posição.
+shuffle($rand); // Para dar um aleatorio de posição para um array.
 
 for ($i=0;$i<10;$i++) {
     echo $rand[$i] . ' ';
