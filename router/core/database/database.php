da95a148da31abe47b57aca326bcea502428b3ad
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-06-07 21:10:39 -0300

    Add files via upload

---
+++ router/core/database/database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Connection {
+    public static function conn($config) {
+
+       try {
+          // $pdo = new PDO('mysql:host=127.0.0.1;dbname=todos','root',''); Não precisa mais.
+        
+            return new PDO(
+                $config['connection'].';dbname='.$config['name'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            ); 
+            
+       } catch(PDOException $e) {
+            die($e->getMessage()); 
+       }
+
+    }
+}
